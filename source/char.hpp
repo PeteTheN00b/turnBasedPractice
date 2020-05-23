9f3a273b3282f8a07fe4cbdd40d6a374fb3fc9cc
PeteTheN00b <ropeter63@googlemail.com>
2020-05-23 14:51:46 +0200

    Rebuilt, added basic Pts structure

---
+++ source/char.hpp
@@ -0,0 +1,20 @@
+#ifndef CHAR_HPP
+#define CHAR_HPP
+
+namespace pawn
+{
+	struct Pts {
+		int amt = 10;
+		int max = 10;
+	};
+
+	class Char {
+		Pts hp_;
+		Pts mp_;
+
+	public:
+
+	};
+}
+
+#endif
